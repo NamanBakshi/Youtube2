e11ee733e12e2ab718de72610f9fb87c3c006d23
Naman Bakshi <bakshinaman26@gmail.com>
2023-04-14 01:09:18 +0530

    few changes

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -3,7 +3,7 @@ import { Typography, Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos } from "./";
+import { Videos } from "./inde";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState(null);
