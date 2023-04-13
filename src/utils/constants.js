4f6a2b6fd521cba7a610b07532d6d888943c24e6
Naman Bakshi <bakshinaman26@gmail.com>
2023-04-14 00:22:39 +0530

    basic layout

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+//import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+//import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+//import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+//export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  // { name: 'JS Mastery', icon: <CodeIcon />, },
+  // { name: 'Coding', icon: <CodeIcon />, },
+  // { name: 'ReactJS', icon: <CodeIcon />, },
+  // { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  // { name: 'Fashion', icon: <CheckroomIcon />, },
+  // { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  // { name: 'Gym', icon: <FitnessCenterIcon />, },
+  // { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://th.bing.com/th/id/R.9a3e7713e6910adc97dc1fa0973714d0?rik=UCUc4kIWfjQtcA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_229205.png&ehk=7dhQ9dv%2fmEMMsytjWp%2fft6JMdRXTnlvo3r9MLQzZJOQ%3d&risl=&pid=ImgRaw&r=0';
+export const demoChannelUrl = '/channel';
+export const demoVideoUrl = '/video/xFFs9UgOAlE' //zukerbeger video link
+//GDa8kZLNhJ4';
+export const demoChannelTitle = 'No channel title to display';
+export const demoVideoTitle = 'No video title to display';
+export const demoProfilePicture = 'https://th.bing.com/th/id/R.9a3e7713e6910adc97dc1fa0973714d0?rik=UCUc4kIWfjQtcA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_229205.png&ehk=7dhQ9dv%2fmEMMsytjWp%2fft6JMdRXTnlvo3r9MLQzZJOQ%3d&risl=&pid=ImgRaw&r=0'
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,23 @@
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+
+const axios = require("axios");
+
+const options = {
+  params: {
+         maxResults: 50
+      }
+    ,
+  headers: {
+    'X-RapidAPI-Key': '51f333c930msh5494e60c5e55ac9p1456bbjsnac2eb0e26da0',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`,options);
+ // const {data}= await axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyD261Twd19U36EDafJ5j8N1A5Nj47634Os&part=snippet&q=${val}&maxResults=50`)
+  //console.log("this is data from get req")
+  console.log(data)
+  return (data);
+};
