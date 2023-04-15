a1e643bb7ec37376382b8304d726480dc6972ef7
Naman Bakshi <bakshinaman26@gmail.com>
2023-04-15 14:21:55 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
 import { Box } from '@mui/material';
 
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
-const App = () => (
-  <BrowserRouter>
-    <Box sx={{ backgroundColor: '#000' }}>
+// const App = () => (
+//   <BrowserRouter>
+//     <Box sx={{ backgroundColor: '#000' }}>
+//       <Navbar />
+//       <Routes>
+//         <Route exact path='/' element={<Feed />} />
+//         <Route path='/video/:id' element={<VideoDetail />} />
+//         <Route path='/channel/:id' element={<ChannelDetail />} />
+//         <Route path='/search/:searchTerm' element={<SearchFeed />} />
+//       </Routes>
+//     </Box>
+//   </BrowserRouter>
+// );
+
+// export default App;
+
+
+
+
+
+
+
+const Layout = () => {
+  return (
+    <div className="w-50 mx-auto">
       <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Feed />} />
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
-      </Routes>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {path:"/",
+  element:<Layout />,
+  children:[
+//}
+      {
+        path: "/",
+        element: <Feed />,
+      },
+      {
+        path: "/video/:id",
+        element: <VideoDetail />,
+      },
+      {
+        path: "/channel/:id",
+        element: <ChannelDetail />,
+      },
+      {
+        path: "/search/:searchTerm",
+        element: <SearchFeed />,
+      }
+    ]}
+  ])
+
+function App() {
+  return (
+    <Box sx={{ backgroundColor: '#000' }}>
+      <RouterProvider router={router}></RouterProvider>
     </Box>
-  </BrowserRouter>
-);
+  );
+}
 
 export default App;
+
